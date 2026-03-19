41dafef787eb0dceef731dc491aca59942018ac2
shendev27 <ostinechen@gmail.com>
2026-03-18 21:30:45 -0400

    general format skeleton

---
+++ main.java
@@ -0,0 +1,14 @@
+public import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int testCases = Integer.parseInt(input.nextLine());
+            for (int testcase = 0; testcase < testCases; testcase++) {
+                
+            }
+        }
+    }
+} {
+    
+}
